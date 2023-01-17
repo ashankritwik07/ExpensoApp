8dd06bcde3e1e3ccf7625573ee3b402bda9751fc
Ashank Ritwik <ashankritwik07@gamil.com>
2023-01-18 01:35:36 +0530

    MY first app

--- app/src/main/java/com/example/foodapp/SignIn_Activity.java
+++ app/src/main/java/com/example/foodapp/SignIn_Activity.java
@@ -2,9 +2,15 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignIn_Activity extends AppCompatActivity {
+
+    Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,15 @@ public class SignIn_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         getSupportActionBar().setTitle("Sign In");
+        signin = findViewById(R.id.btnsignin);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignIn_Activity.this, "logged In", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignIn_Activity.this,Acitivity_mainpage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/foodapp/SignUp_Acitivity.java
+++ app/src/main/java/com/example/foodapp/SignUp_Acitivity.java
@@ -2,9 +2,14 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUp_Acitivity extends AppCompatActivity {
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,15 @@ public class SignUp_Acitivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up_acitivity);
 
         getSupportActionBar().setTitle("Sign Up");
+        register = findViewById(R.id.btnregister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignUp_Acitivity.this, "registered", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignUp_Acitivity .this ,Acitivity_mainpage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
